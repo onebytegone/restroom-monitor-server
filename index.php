402e4005caa8d177bca74ab0b136020d637cf822
Ethan Smith <ethan@onebytegone.com>
2015-05-16 08:21:43 -0400

    Allowed return of JSON and JSONP

--- index.php
+++ index.php
@@ -7,16 +7,21 @@ $app = new \Slim\Slim();
 
 $dataHistory = new DataHistory('data/statusdata.txt');
 
+function outputJSONP($app, $json) {
+   $app->contentType('application/javascript');
+   $callback = $app->request()->get('callback');
+   if ($callback) {
+      echo "{$callback}($json)";
+   } else {
+      echo $json;
+   }
+}
 
 $app->get('/', function () use ($dataHistory, $app) {
    $data = $dataHistory->getItems(1)[0];
    $json = json_encode($data);
 
-
-   $callback = $app->request()->get('callback');
-   $app->contentType('application/javascript');
-   echo "{$callback}($json)";
+   outputJSONP($app, $json);
 });
-
 
 $app->run();
