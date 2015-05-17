0d569b25ca194380a12ac843c9f0744958881348
Ethan Smith <ethan@onebytegone.com>
2015-05-16 21:31:16 -0400

    Added start for updated endpoint

--- environment.example.php
+++ environment.example.php
@@ -17,3 +17,7 @@ date_default_timezone_set("America/New_York");
  */
 define('ROOT_DIR', dirname(__FILE__));
 
+/**
+ * This is the key to share between the server and the status updater
+ */
+define('PRIVATE_SHARED_JWT_KEY', 'correcthorsebatterystaple');
